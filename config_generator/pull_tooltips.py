21a1bd229340891f7c89375ae35d06665abbf668
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-17 15:53:50 -0300

    feat: Bring updates from other repository
    From https://github.com/thecloudcircle/sparnatural-config-generator .
    Namely:
    
    - Reverted disease QID to older QID
    - Fixed Disease-Gene relation
    - Implemented Date support
    - Impemented Text support

--- config_generator/pull_tooltips.py
+++ config_generator/pull_tooltips.py
@@ -1,27 +1,25 @@
-import pandas as pd 
+import pandas as pd
 from wikidata2df import wikidata2df
 
 
-def get_tooltips_dict(properties_df):
+def get_tooltips_dict(list_of_wikidata_ids):
 
-  prop_strings = properties_df["id"]
-
-  query = f"""
+    query = f"""
   SELECT 
     ?prop_string 
     ?prop 
     ?propLabel
     ?propDescription
   WHERE{{
-    VALUES ?prop_string   {{ "{'" "'.join(prop_strings)}" }}
+    VALUES ?prop_string   {{ "{'" "'.join(list_of_wikidata_ids)}" }}
     BIND (IRI (CONCAT("http://www.wikidata.org/entity/",?prop_string)) as ?prop)
     SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
     }}
   """
 
-  tooltips_df = wikidata2df(query)
-  tooltips_dict = {}
-  for i, row in tooltips_df.iterrows():
-    tooltips_dict[row.prop_string] = row.propDescription
+    tooltips_df = wikidata2df(query)
+    tooltips_dict = {}
+    for i, row in tooltips_df.iterrows():
+        tooltips_dict[row.prop_string] = row.propDescription
 
-  return tooltips_dict
+    return tooltips_dict
