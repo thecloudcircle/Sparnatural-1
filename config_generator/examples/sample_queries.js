0f0360f830e2b5cf9eb7507edb861782cfd9323b
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-07-01 17:09:38 -0300

    Update config

--- config_generator/examples/sample_queries.js
+++ config_generator/examples/sample_queries.js
@@ -250,6 +250,86 @@ var sampleQueries = {
         "children": []
       }
     ]
+  },
+  // Phase III Clinical Trials for drugs that bind EGFR
+  "example.5": {
+    "distinct": true,
+    "variables": [
+      "?this",
+      "?Chemical_Compound_1",
+      "?Disease_3"
+    ],
+    "defaultLang": "en",
+    "order": null,
+    "branches": [
+      {
+        "line": {
+          "s": "?this",
+          "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#clinical_trial_phase",
+          "o": "?Phase_of_Clinical_Research_1",
+          "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Clinical_Trial",
+          "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Phase_of_Clinical_Research",
+          "values": [
+            {
+              "label": "phase III clinical trial",
+              "uri": "http://www.wikidata.org/entity/Q42824827"
+            }
+          ]
+        },
+        "children": []
+      },
+      {
+        "line": {
+          "s": "?this",
+          "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#research_intervention",
+          "o": "?Chemical_Compound_1",
+          "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Clinical_Trial",
+          "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Chemical_Compound",
+          "values": []
+        },
+        "children": [
+          {
+            "line": {
+              "s": "?Chemical_Compound_1",
+              "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#binds_protein",
+              "o": "?Protein_1",
+              "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Chemical_Compound",
+              "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Protein",
+              "values": []
+            },
+            "children": [
+              {
+                "line": {
+                  "s": "?Protein_1",
+                  "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#encoded_by",
+                  "o": "?Gene_2",
+                  "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Protein",
+                  "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Gene",
+                  "values": [
+                    {
+                      "label": "EGFR",
+                      "uri": "http://www.wikidata.org/entity/Q14865565"
+                    }
+                  ]
+                },
+                "children": []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "line": {
+          "s": "?this",
+          "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#medical_condition",
+          "o": "?Disease_3",
+          "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Clinical_Trial",
+          "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Disease",
+          "values": []
+        },
+        "children": []
+      }
+    ]
   }
 
 };
